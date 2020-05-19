2a73585f3707cc256469cf6b34bb1055c25a3ea6
Luca Di Stefano <luca.distefano@gssi.it>
2020-05-19 15:45:49 +0200

    Fix HISTORY, update __about__.py

--- __about__.py
+++ __about__.py
@@ -3,8 +3,8 @@ __all__ = [
 ]
 
 __title__ = "SLiVER"
-__summary__ = "Symbolic LAbS VERification"
+__summary__ = "The SLiVER LAbS VERification tool"
 __uri__ = "https://github.com/labs-lang/sliver"
 
-__version__ = "1.4"
-__date__ = "February 2020"
+__version__ = "1.5"
+__date__ = "May 2020"
