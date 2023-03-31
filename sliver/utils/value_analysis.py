d338d00b3c7d132ba5e3aed9989459bc4ddb9a77
Luca Di Stefano <luca.distefano@gssi.it>
2023-03-31 09:25:05 +0200

    Changes in value_analysis
    * Interpretation of statements now returns None if the result state is
      already known
    * Refactor guard sensitivity (we now use a concurrent apply_guard based
      on bisection rather than enumeration)
    * id is no longer an extern but part of State (this is needed to apply
      guards)
    * Use new ast node API
    * Fix eval_expr()
    * Cleanup
    
    Update labsparse

--- sliver/utils/value_analysis.py
+++ sliver/utils/value_analysis.py
@@ -2,13 +2,13 @@
 # Rudimentary value analysis for LAbS specifications
 
 from collections import defaultdict, namedtuple
-from functools import reduce
-from itertools import permutations, product
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from functools import reduce, lru_cache
+from itertools import permutations, product
 
 from ..app.cli import Args
+from ..labsparse.labs_ast import Expr, QueryResult
 from ..labsparse.labs_parser import Attr, NodeType, parse_to_dict
-from ..labsparse.labs_ast import Expr, Node
 
 
 class Interval:
@@ -159,8 +159,6 @@ class Stripes:
         self.stripes = frozenset(args)
 
     def extrema(self):
-        # if not self.stripes:
-        #     return None
         return (
             min(i.min for i in self.stripes),
             max(i.max for i in self.stripes))
@@ -178,6 +176,7 @@ class Stripes:
                 return Stripes(i0), Stripes(i1)
             else:
                 return None, None
+
     @staticmethod
     def _prune(stripes: set, prune_adjacent=False) -> frozenset:
         # enter = True
@@ -342,6 +341,16 @@ def merge(s0, s1, State):
     return State(**result)
 
 
+def entailed_by(s1, states):
+    for s2 in states:
+        if s1 == s2:
+            continue
+        if all(getattr(s1, var).is_within(getattr(s2, var))
+               for var in s1._fields):
+            return True
+    return False
+
+
 def make_init(info, local_names):
     """Get value analysis in the initial state"""
     stores = (info.lstig, info.e, *(a.iface for a in info.spawn.values()))
@@ -359,10 +368,10 @@ def make_init(info, local_names):
                 else:
                     s0[var.name] = stripe
 
-    names = [*local_names, *s0.keys()]
-    State = namedtuple("State", names)
+    s0["id"] = S(0, info.spawn.num_agents() - 1)
+    State = namedtuple("State", [*local_names, *s0.keys(), "id"])
     for x in local_names:
-        s0[x] = Stripes(I(0))
+        s0[x] = NO
     s0 = State(**s0)
 
     return State, s0
@@ -386,7 +395,7 @@ def eval_expr(expr, s, externs) -> Stripes:
         ">=": lambda x, y: x >= y,
         "<=": lambda x, y: x <= y,
         "=": lambda x, y: x.equality(y),
-        "!=": lambda x, y: not x.equality(y),
+        "!=": lambda x, y: ~x.equality(y),
     }
     UNARY_OPS = {
         "unary-minus": lambda x: -x,
@@ -394,23 +403,22 @@ def eval_expr(expr, s, externs) -> Stripes:
         "abs": abs
     }
 
-    if expr.matches(NodeType.LITERAL):
+    if expr(NodeType.LITERAL):
         return S(expr[Attr.VALUE])
-    elif expr.matches(NodeType.REF) and expr[Attr.NAME] in s._fields:
+    elif expr(NodeType.REF) and expr[Attr.NAME] in s._fields:
         return getattr(s, expr[Attr.NAME])
-    elif expr.matches(NodeType.REF) or expr.matches(NodeType.REF_EXT):
+    elif expr(NodeType.REF) or expr(NodeType.REF_EXT):
         return externs[expr[Attr.NAME]]
-    elif expr.matches(NodeType.BUILTIN) and expr[Attr.NAME] in UNARY_OPS:
+    elif expr(NodeType.BUILTIN) and expr[Attr.NAME] in UNARY_OPS:
         return UNARY_OPS[expr[Attr.NAME]](eval_expr(expr[Attr.OPERANDS][0], s, externs))  # noqa: E501
-    elif (expr.matches(NodeType.EXPR) or expr.matches(NodeType.BUILTIN)
+    elif (expr(NodeType.EXPR) or expr(NodeType.BUILTIN)
             and expr[Attr.NAME] in OPS):
-        operands = (eval_expr(e, s, externs) for e in expr[Attr.OPERANDS])
+        operands = [eval_expr(e, s, externs) for e in expr[Attr.OPERANDS]]
         return reduce(OPS[expr[Attr.NAME]], operands)
-
-    elif expr.matches(NodeType.COMPARISON) and expr[Attr.NAME] in CMP:
+    elif expr(NodeType.COMPARISON) and expr[Attr.NAME] in CMP:
         operands = [eval_expr(e, s, externs) for e in expr[Attr.OPERANDS]]
         return reduce(CMP[expr[Attr.NAME]], operands)
-    elif expr.matches(NodeType.IF):
+    elif expr(NodeType.IF):
         condition = eval_expr(expr[Attr.CONDITION], s, externs)
         if 1 in condition and 0 in condition:
             return (
@@ -422,14 +430,27 @@ def eval_expr(expr, s, externs) -> Stripes:
             return eval_expr(expr[Attr.ELSE], s, externs)
         else:
             raise ValueError(expr.as_labs())
-    elif expr.matches(NodeType.QFORMULA):
+    elif expr(NodeType.QFORMULA):
         # TODO can we recover some information?
         return S(0, 1)
     else:
         raise NotImplementedError(expr.as_labs())
 
 
+@lru_cache(maxsize=256)
+def bisect_by(s0, var, State):
+    s1 = {k: getattr(s0, k) for k in s0._fields}
+    s2 = {k: getattr(s0, k) for k in s0._fields}
+    var1, var2 = getattr(s0, var).bisect()
+    if var1 is None:
+        return None, None
+    s1[var] = var1
+    s2[var] = var2
+    return State(**s1), State(**s2)
+
+
 def apply_guard(guards, stmt, s0, externs, State):
+    """Filters s0 down to the abstract state where stmt's guard always holds"""
     g = guards.get(stmt)
     if g is None:
         return s0
@@ -437,23 +458,35 @@ def apply_guard(guards, stmt, s0, externs, State):
     new_guard[Attr.SYNTHETIC] = True
     new_guard[Attr.NAME] = "and"
     new_guard[Attr.OPERANDS] = g
-    eval_whole = eval_expr(new_guard, s0, externs)
-    if 1 in eval_whole and 0 not in eval_whole:
-        # Guard always holds
-        return s0
-    elif 0 in eval_whole and 1 not in eval_whole:
-        # Guard never holds
-        return None
-    # If we are here we must filter s0
-    # TODO: do recursive bisection rather than enumeration
-    s1 = None
-    for s in enumerate(s0, State):
-        if 1 in eval_expr(new_guard, s, externs):
-            s1 = s if s1 is None else merge(s, s1, State)
-    return s1
+    g_vars = set(x[Attr.NAME] for x in (new_guard // (NodeType.REF, )))
+
+    def recursive_apply(s):
+        eval_whole = eval_expr(new_guard, s, externs)
+        if 1 in eval_whole and 0 not in eval_whole:
+            # Guard always holds
+            return s
+        elif 0 in eval_whole and 1 not in eval_whole:
+            # Guard never holds
+            return None
+        bisections = [x for v in g_vars for x in bisect_by(s, v, State)]
+        bisections = [x for x in bisections if x is not None]
+        if len(bisections) == 0:
+            raise ValueError(f"This should be unreachable {s}")
+
+        with ThreadPoolExecutor() as exc:
+            recurse = exc.map(recursive_apply, bisections)
+        recurse = [x for x in recurse if x is not None]
+        if len(recurse) == 1:
+            return next(iter(recurse))
+        elif len(recurse) > 1:
+            return reduce(lambda x0, x1: merge(x0, x1, State), recurse)
+        else:
+            raise ValueError(f"This should be unreachable {s} {new_guard.as_labs()} {eval_whole}")
+
+    return recursive_apply(s0)
 
 
-def apply_assignment(asgn, s0, externs, guards, State):
+def apply_assignment(asgn, s0, externs, guards, old, State):
     # If asgn is guarded, reduce s0 to the states where the guard holds
     s0 = apply_guard(guards, asgn, s0, externs, State)
     # The guard did not hold anywhere, so we cannot interpret asgn
@@ -462,52 +495,78 @@ def apply_assignment(asgn, s0, externs, guards, State):
     s1 = {k: getattr(s0, k) for k in s0._fields}
     for lhs, rhs in zip(asgn[Attr.LHS], asgn[Attr.RHS]):
         s1[lhs[Attr.NAME]] = eval_expr(rhs, s0, externs)
-    return State(**s1)
+    s1 = State(**s1)
+    return None if old is not None and entailed_by(s1, old) else s1
 
 
-def apply_block(blk, s0, externs, guards, State):
+def apply_block(blk, s0, externs, guards, old, State):
     # If blk is guarded, reduce s0 to the states where the guard holds
     s0 = apply_guard(guards, blk, s0, externs, State)
     # The guard did not hold anywhere, so we cannot interpret blk
     if s0 is None:
         return None
     for asgn in blk[Attr.BODY]:
-        s1 = apply_assignment(asgn, s0, externs, guards, State)
+        s1 = apply_assignment(asgn, s0, externs, guards, None, State)
         s0 = s1
-    return s1
+    return None if old is not None and entailed_by(s1, old) else s1
+
+
+def dependency_analysis(assignments, blocks):
+    depends = defaultdict(set)
+
+    def update_depends(assignment):
+        for lhs, rhs in zip(assignment[Attr.LHS], assignment[Attr.RHS]):
+            # # We skip conditions (on ifs), since they don't really
+            # # affect the values
+            rhs1 = QueryResult(rhs.walk(ignore_attrs=[Attr.CONDITION]))
+            names = [x[Attr.NAME] for x in rhs1 // (NodeType.REF, )]
+            depends[lhs[Attr.NAME]].update(names)
+
+    for a in assignments:
+        update_depends(a)
+    for blk in blocks:
+        for a in (blk // (NodeType.ASSIGN, )):
+            update_depends(a)
+
+    # Transitivity
+    old_depends = None
+    while old_depends != depends:
+        old_depends = {**depends}
+        for v, v_deps in old_depends.items():
+            for w in v_deps:
+                depends[v].update(old_depends.get(w, []))
+    return depends
 
 
 def value_analysis(cli, info):
-    externs = {
-        "id": S(0, info.spawn.num_agents() - 1)
-    }
+    externs = {}
     for ext in cli[Args.VALUES]:
         name, value = ext.split("=")
         externs["_" + name] = S(int(value))
     ast = parse_to_dict(cli.file)
 
-    # Extract all assignment, blocks, and guards from specification.
+    # Extract all assignment, blocks, and guards
     # (That are reachable from the agents' behaviour)
-    assignments = []
-    blocks = []
-    guards = []
+    assignments = set()
+    blocks = set()
+    guards = set()
     for a in ast.agents:
         behavior = [
             p for p in a[Attr.PROCDEFS] if p[Attr.NAME] == "Behavior"][0]
-        visited_calls = set(("Behavior", ))
+        visited_calls = set(("Behavior", "Skip"))
         frontier = [behavior]
         while frontier:
             node = frontier.pop()
-            assignments.extend(
-                n for n in node.walk([NodeType.BLOCK])
-                if n.matches(NodeType.ASSIGN))
-            blocks.extend(n for n in node.walk() if n.matches(NodeType.BLOCK))
-            guards.extend(
+            assignments.update(
+                n for n in node.walk(ignore_types=[NodeType.BLOCK])
+                if n(NodeType.ASSIGN))
+            blocks.update(n for n in node.walk() if n(NodeType.BLOCK))
+            guards.update(
                 (a, n) for n in node.walk()
-                if n.matches(NodeType.GUARDED))
+                if n(NodeType.GUARDED))
             calls = (
                 n[Attr.NAME] for n in node.walk()
-                if n.matches(NodeType.CALL)
+                if n(NodeType.CALL)
                 and n[Attr.NAME] not in visited_calls)
             for c in calls:
                 visited_calls.add(c)
@@ -518,21 +577,21 @@ def value_analysis(cli, info):
 
     # Map assignments/blocks to their guards
     def get_guarded_assignments(node, agent):
-        if node == "Skip":
-            return set()
-        elif node.matches(NodeType.COMPOSITION):
+        if node(NodeType.COMPOSITION):
             if node[Attr.NAME] == "seq":
                 return get_guarded_assignments(node[Attr.OPERANDS][0], agent)
             else:
                 return set.union(*(
                     get_guarded_assignments(n, agent)
                     for n in node[Attr.OPERANDS]))
-        elif node.matches(NodeType.ASSIGN) or node.matches(NodeType.BLOCK):
+        elif node(NodeType.ASSIGN) or node(NodeType.BLOCK):
             return set((node, ))
-        elif node.matches(NodeType.GUARDED):
+        elif node(NodeType.GUARDED):
             return get_guarded_assignments(node[Attr.BODY], agent)
-        elif node.matches(NodeType.CALL):
+        elif node(NodeType.CALL):
             name = node[Attr.NAME]
+            if name == "Skip":
+                return set()
             p = agent.try_lookup(name) or ast["system"].try_lookup(name)
             if p is None:
                 raise KeyError(name)
@@ -546,6 +605,8 @@ def value_analysis(cli, info):
         for x in get_guarded_assignments(g[Attr.BODY], agent):
             guard_map[x].append(g[Attr.CONDITION])
 
+    depends = dependency_analysis(assignments, blocks)
+
     # Retrieve local variable names and build initial state
     local_names = [
         lhs[Attr.NAME]
@@ -555,38 +616,31 @@ def value_analysis(cli, info):
         if n[Attr.TYPE] == "local"]
     State, s0 = make_init(info, local_names)
 
-    def entailed_by(s1, s2):
-        if s1 == s2:
-            return False
-        return all(
-            getattr(s1, var).is_within(getattr(s2, var))
-            for var in s1._fields)
-
-    def parallel_find_entailed(set1, set2, exc):
-        def find_entailed_1(s, states):
-            for s1 in states:
-                if entailed_by(s1, s):
-                    return s1
-            return None
-        futures = [exc.submit(lambda: find_entailed_1(s, set2)) for s in set1]
-        for f in as_completed(futures):
-            s = f.result()
-            if s is not None:
-                yield s
-
     # We use a chaos automaton of all assignments/blocks
     # to overapproximate the range of feasible values
     fixpoint = False
     old_states = set([s0])
-    # TODO make analysis bound configurable
 
+    def lhs_of(asgn_or_blk):
+        if asgn_or_blk(NodeType.ASSIGN):
+            return set(n[Attr.NAME] for n in asgn_or_blk[Attr.LHS])
+        elif asgn_or_blk(NodeType.BLOCK):
+            return set().union(*(lhs_of(a) for a in (asgn_or_blk // (NodeType.ASSIGN, ))))  # noqa: E501
+
+    def mergeStates(s0, s1):
+        return merge(s0, s1, State)
+
+    wont_change = set()
     with ThreadPoolExecutor() as exc:
-        for i in range(20):
+        # TODO make analysis bound configurable
+        for i in range(10):
+            print()
+            print("loop", i, len(old_states))
             # Prune away old states entailed by others
-            old_states -= set(parallel_find_entailed(old_states, old_states, exc))  # noqa: E501
+            old_merge = reduce(mergeStates, old_states)
 
             futures = []
-            common_args = (externs, guard_map, State)
+            common_args = (externs, guard_map, old_states, State)
             futures = [
                 exc.submit(apply_assignment, a, s, *common_args)
                 for a, s in product(assignments, old_states)]
@@ -595,20 +649,42 @@ def value_analysis(cli, info):
                 for blk, s in product(blocks, old_states))
 
             new_states = (f.result() for f in as_completed(futures))
-            new_states = set(x for x in new_states if x is not None)
-            new_states -= set(parallel_find_entailed(new_states, new_states, exc))  # noqa: E501
+            new_states = set(s for s in new_states if s is not None)
 
             if new_states <= old_states:
                 fixpoint = True
                 break
-            else:
-                old_states |= new_states
+            old_states |= new_states
 
-    def mergeStates(s0, s1):
-        return merge(s0, s1, State)
+            # Detect which variables won't change anymore
+            next_merge = reduce(mergeStates, old_states)
+            common_args = (externs, {}, (next_merge, ), State)
+            no_dep_vars = [v for v in depends if len(depends[v]) == 0]
+            for v in no_dep_vars:
+                futures = [
+                    exc.submit(apply_assignment, a, s, *common_args)
+                    for s in old_states
+                    for a in assignments
+                    if any(lhs == v for lhs in lhs_of(a))]
+                futures.extend(
+                    exc.submit(apply_block, b, s, *common_args)
+                    for s in old_states
+                    for b in blocks
+                    if any(lhs == v for lhs in lhs_of(b)))
+            result = (f.result() for f in as_completed(futures))
+            result = [s for s in result if s is not None]
+            next2_merge = (
+                reduce(mergeStates, result)
+                if len(result)
+                else next_merge)
+            new_wont_change = [
+                v for v in no_dep_vars
+                if getattr(next2_merge, v).is_within(getattr(next_merge, v))
+                and getattr(next_merge, v).is_within(getattr(old_merge, v))
+                and v not in wont_change]
+            wont_change.update(new_wont_change)
 
-    # print(*old_states, sep="\n")
-    s1 = reduce(mergeStates, old_states)
+    s1 = old_merge if fixpoint else reduce(mergeStates, old_states)
     s1 = State(**{
         x: getattr(s1, x).join_adjacent()
         for x in s1._fields
