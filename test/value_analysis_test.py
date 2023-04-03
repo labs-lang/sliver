44ed620f4e1c307fdccb82ac02bdb96f90ef2291
Luca Di Stefano <luca.distefano@gssi.it>
2023-04-03 10:46:04 +0200

    Add tests for is_within()

--- test/value_analysis_test.py
+++ test/value_analysis_test.py
@@ -2,7 +2,13 @@ import unittest
 from sliver.utils.value_analysis import YES, NO, MAYBE, S
 
 
+S4_5 = S(4, 5)
+S4 = S(4)
+S5 = S(5)
+S6 = S(6)
+
 class TestStripes(unittest.TestCase):
+    
     def setUp(self) -> None:
         pass
 
@@ -10,7 +16,7 @@ class TestStripes(unittest.TestCase):
         self.assertEqual(NO < YES, YES)
         self.assertEqual(NO < NO, NO)
         self.assertEqual(NO < MAYBE, MAYBE)
-        
+
         self.assertEqual(YES < YES, NO)
         self.assertEqual(YES < NO, NO)
         self.assertEqual(YES < MAYBE, NO)
@@ -19,8 +25,25 @@ class TestStripes(unittest.TestCase):
         self.assertEqual(MAYBE < NO, NO)
         self.assertEqual(MAYBE < MAYBE, MAYBE)
 
-        self.assertEqual(S(4, 5) < S(6), YES)
-        self.assertEqual(S(4, 5) < S(5), MAYBE)
-        self.assertEqual(S(4) < S(4, 5), MAYBE)
-        self.assertEqual(S(5) < S(4, 5), NO)
-        self.assertEqual(S(6) < S(4, 5), NO)
+        self.assertEqual(S4_5 < S6, YES)
+        self.assertEqual(S4_5 < S5, MAYBE)
+        self.assertEqual(S4 < S4_5, MAYBE)
+        self.assertEqual(S5 < S4_5, NO)
+        self.assertEqual(S6 < S4_5, NO)
+
+    def test_is_within(self):
+        self.assertTrue(YES.is_within(YES))
+        self.assertTrue(NO.is_within(NO))
+        self.assertTrue(MAYBE.is_within(MAYBE))
+
+        self.assertTrue(YES.is_within(MAYBE))
+        self.assertTrue(NO.is_within(MAYBE))
+
+        self.assertFalse(YES.is_within(NO))
+        self.assertFalse(NO.is_within(YES))
+        self.assertFalse(MAYBE.is_within(YES))
+        self.assertFalse(MAYBE.is_within(NO))
+
+        self.assertTrue(S4.is_within(S4_5))
+        self.assertTrue(S5.is_within(S4_5))
+        self.assertFalse(S6.is_within(S4_5))
