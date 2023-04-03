917379f0f324045d0b9e88818a9515ec907278bb
Luca Di Stefano <luca.distefano@gssi.it>
2023-04-03 11:26:07 +0200

    Update version number

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 
-            SLiVER  4.0-230310
-               10 March 2023
+            SLiVER  4.0-230330
+               30 March 2023
 
 The SLiVER LAbS VERification tool
 
--- sliver/app/__about__.py
+++ sliver/app/__about__.py
@@ -6,5 +6,5 @@ __title__ = "SLiVER"
 __summary__ = "The SLiVER LAbS VERification tool"
 __uri__ = "https://github.com/labs-lang/sliver"
 
-__version__ = "4.0-230310"
-__date__ = "10 March 2023"
+__version__ = "4.0-230330"
+__date__ = "30 March 2023"
