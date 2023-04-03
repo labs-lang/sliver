363a856db064b9f4471e1d7eb0e9b99780f4f2a8
Luca Di Stefano <luca.distefano@gssi.it>
2023-04-03 15:56:36 +0200

    Sign tests

--- test/value_analysis_test.py
+++ test/value_analysis_test.py
@@ -1,6 +1,6 @@
 import unittest
-from sliver.analysis.domains import Stripes
 
+from sliver.analysis.domains import Sign, Stripes
 
 S4_5 = Stripes.abstract(4, 5)
 S4 = Stripes.abstract(4)
@@ -47,3 +47,81 @@ class TestStripes(unittest.TestCase):
         self.assertTrue(S4.is_within(S4_5))
         self.assertTrue(S5.is_within(S4_5))
         self.assertFalse(S6.is_within(S4_5))
+
+
+NATURAL = Sign(plus=True, zero=True)
+NONPOSITIVE = Sign(zero=True, minus=True)
+NONZERO = Sign(plus=True, minus=True)
+
+
+class TestSign(unittest.TestCase):
+
+    def test_invert(self):
+        self.assertEqual(~Sign.YES, Sign.NO)
+        self.assertEqual(~Sign.NO, Sign.YES)
+        self.assertEqual(~Sign.MAYBE, Sign.MAYBE)
+
+    def test_add(self):
+        self.assertEqual(Sign.YES + NATURAL, Sign.YES)
+        self.assertEqual(Sign.YES + Sign.NEG, Sign.TOP)
+        self.assertEqual(Sign.YES + NONPOSITIVE, Sign.TOP)
+        self.assertEqual(Sign.YES + NONZERO, Sign.TOP)
+        self.assertEqual(Sign.YES + Sign.YES, Sign.YES)
+        self.assertEqual(Sign.YES + Sign.TOP, Sign.TOP)
+        self.assertEqual(Sign.YES + Sign.NO, Sign.YES)
+
+        self.assertEqual(Sign.NEG + NATURAL, Sign.TOP)
+        self.assertEqual(Sign.NEG + Sign.NEG, Sign.NEG)
+        self.assertEqual(Sign.NEG + NONPOSITIVE, Sign.NEG)
+        self.assertEqual(Sign.NEG + NONZERO, Sign.TOP)
+        self.assertEqual(Sign.NEG + Sign.YES, Sign.TOP)
+        self.assertEqual(Sign.NEG + Sign.TOP, Sign.TOP)
+        self.assertEqual(Sign.NEG + Sign.NO, Sign.NEG)
+
+        self.assertEqual(Sign.NO + Sign.NO, Sign.NO)
+        self.assertEqual(Sign.TOP + Sign.TOP, Sign.TOP)
+
+    def test_mul(self):
+        self.assertEqual(Sign.YES * Sign.YES, Sign.YES)
+        self.assertEqual(Sign.YES * Sign.NO, Sign.NO)
+        self.assertEqual(Sign.YES * Sign.NEG, Sign.NEG)
+        self.assertEqual(Sign.NEG * Sign.YES, Sign.NEG)
+        self.assertEqual(Sign.NEG * Sign.NEG, Sign.YES)
+        self.assertEqual(Sign.NEG * Sign.NO, Sign.NO)
+        self.assertEqual(Sign.NO * Sign.NO, Sign.NO)
+
+    def test_or(self):
+        self.assertEqual(Sign.YES | Sign.YES, Sign.YES)
+        self.assertEqual(Sign.YES | Sign.NO, NATURAL)
+        self.assertEqual(Sign.YES | Sign.NEG, NONZERO)
+        self.assertEqual(Sign.NEG | Sign.YES, NONZERO)
+        self.assertEqual(Sign.NEG | Sign.NEG, Sign.NEG)
+        self.assertEqual(Sign.NEG | Sign.NO, NONPOSITIVE)
+        self.assertEqual(Sign.NO | Sign.NO, Sign.NO)
+
+    def test_neg(self):
+        self.assertEqual(-NATURAL, NONPOSITIVE)
+        self.assertEqual(-Sign.NEG, Sign.YES)
+        self.assertEqual(-NONPOSITIVE, NATURAL)
+        self.assertEqual(-NONZERO, NONZERO)
+        self.assertEqual(-Sign.YES, Sign.NEG)
+        self.assertEqual(-Sign.TOP, Sign.TOP)
+        self.assertEqual(-Sign.NO, Sign.NO)
+
+    def test_min(self):
+        self.assertEqual(Sign.YES.Min(Sign.YES), Sign.YES)
+        self.assertEqual(Sign.YES.Min(Sign.NO), Sign.NO)
+        self.assertEqual(Sign.YES.Min(Sign.NEG), Sign.NEG)
+        self.assertEqual(Sign.NEG.Min(Sign.YES), Sign.NEG)
+        self.assertEqual(Sign.NEG.Min(Sign.NEG), Sign.NEG)
+        self.assertEqual(Sign.NEG.Min(Sign.NO), Sign.NEG)
+        self.assertEqual(Sign.NO.Min(Sign.NO), Sign.NO)
+
+    def test_max(self):
+        self.assertEqual(Sign.YES.Max(Sign.YES), Sign.YES)
+        self.assertEqual(Sign.YES.Max(Sign.NO), Sign.YES)
+        self.assertEqual(Sign.YES.Max(Sign.NEG), Sign.YES)
+        self.assertEqual(Sign.NEG.Max(Sign.YES), Sign.YES)
+        self.assertEqual(Sign.NEG.Max(Sign.NEG), Sign.NEG)
+        self.assertEqual(Sign.NEG.Max(Sign.NO), Sign.NO)
+        self.assertEqual(Sign.NO.Max(Sign.NO), Sign.NO)
